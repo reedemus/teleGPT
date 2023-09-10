defc2ae67ac8231c30d7fe06bbcd1b04b20d5b7d
Terrence Goh <reedemus@yahoo.com.sg>
2023-09-10 23:07:42 +0800

    add chatGPT model support, requirements.txt

--- app.py
+++ app.py
@@ -1,27 +1,32 @@
-import openai, os
+import os
+import model_openai
 from dotenv import load_dotenv, find_dotenv
 from telegram import Update
 from telegram.ext import Application, ContextTypes, MessageHandler, CommandHandler, filters
 
 # Get API tokens from environment file
 _ = load_dotenv(find_dotenv())
-openai.api_key = os.getenv('OPENAI_API_TOKEN')
 TELE_API_KEY = os.getenv('TELEGRAM_API_TOKEN')
 BOT_NAME = '@gpt123bot' 
 
-
 # /start cmd handler
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text('Type stg to get started!')
+    """Return a string when start command is received"""
+    await update.message.reply_text(f"Hi, I'm a chatbot powered by {llm.name}. Ask me anything.")
 
 
 # Command handler
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text('Ask Google loh!')
+    """Return helpful information when help command is received"""
+    await update.message.reply_text(
+        'I can summarize paragraphs of text, translate any \
+        sentence, generate contents such as poems, lyrics, \
+        or email. Ask me anything.')
 
 
 # Custom command handler
 async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Return a string when custom command is received"""
     await update.message.reply_text('Custom cmd!')
 
 
@@ -32,22 +37,20 @@ async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 # Response handler
 def handle_response(msg: str) -> str:
-    msg_lowercase = msg.lower()
-
-    if 'hello' in msg_lowercase:
-        response = 'Nice to meet you'
-        return response
-
-    if 'happy' in msg_lowercase:
-        response = 'happy to meet you too'
-        return response
-
-    # If not getting expected responses, return default msg
-    return "I don't understand"
+    return llm.handle_response(msg)
 
 
 # Input message handler
-async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Message handler
+
+    update: Update object containing message details such as user id, message contents, etc.
+    context: Context object
+
+    TODO :
+    1) handle multiple users' responses - track each user's chat history from Update user id
+    2) handle streaming response from chatGPT API (stream = true)
+    """
     msg_type = update.message.chat.type
     msg = update.message.text           # Incoming message
 
@@ -65,12 +68,13 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
         response = handle_response(msg)
 
     # print response message for debug
-    print('Bot response:', response)
+    print(f'Bot: "{response}"')
     await update.message.reply_text(response)
 
 
 if __name__ == '__main__':
     print('Bot started...')
+    llm = model_openai.ChatGPT()
     app = Application.builder().token(TELE_API_KEY).build()
 
     # Commands
---
+++ model_openai.py
@@ -0,0 +1,44 @@
+import openai, os
+from dotenv import load_dotenv, find_dotenv
+
+# Get API tokens from environment file
+_ = load_dotenv(find_dotenv())
+openai.api_key = os.getenv('OPENAI_API_TOKEN')
+
+class ChatGPT:
+    def __init__(self, model="gpt-3.5-turbo") -> None:
+        print("chatGPT initialized.")
+        self._name = "chatGPT-3.5"
+        self._model = model
+        self._messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+        ]
+
+
+    # a getter function
+    @property
+    def name(self) -> str:
+        return self._name    
+
+
+    def handle_response(self, prompt: str) -> str:
+        """Retain user messages and append assistant's responses in a message list.
+        This is due to the model does not store chat history after each query is sent.
+        
+        prompt: user input message
+        return: string response from model
+        
+        TODO:
+         1) use moderation API to detect hate speech and adhere to safety guidelines
+         2) prevent prompt injection attacks
+        """
+        self._messages.append({"role": "user", "content": f"{prompt}"})
+        response = openai.ChatCompletion.create(
+            model=self._model,
+            messages=self._messages,
+            temperature=0,
+        )
+        self._messages.append({"role":"assistant", "content":f"{response}"})
+
+        # return assistant's response
+        return response.choices[0].message["content"]
