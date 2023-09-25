105ce0310d2e2c2e643951a5a72d99757e9e9b95
Terrence Goh <reedemus@yahoo.com.sg>
2023-09-26 07:35:56 +0800

    Move user message history out of llm class

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import logging
 import model_openai
 from dotenv import load_dotenv, find_dotenv
+from enum import Enum, auto
 from telegram import Update, constants
 from telegram.ext import (
     Application,
@@ -23,6 +24,69 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
+class UserEnum(Enum):
+    UNKNOWN_USER = auto()
+
+# List of dictionaries of format:
+# [  
+#  { "user": user_id, "messages": user_messages },
+# ]
+users_chat_history = [{"user": UserEnum.UNKNOWN_USER}]
+
+
+class ChatHistory():
+    def __init__(self):
+        self._message = [{"role": "system", "content": "You are a helpful assistant."}]
+
+    # a getter function
+    @property
+    def message(self):
+        """Returns list of messages
+
+        Args:
+            None
+
+        Returns:
+            list
+        """
+        return self._message
+
+    # setter function
+    @message.setter
+    def message(self, msg):
+        self._message = msg
+
+
+def update_chat_history(user_id: int, user_messages: ChatHistory) -> None:
+    """Update chat history of the user"""
+    found = False
+    for idx, _ in enumerate(users_chat_history):
+        if user_id == users_chat_history[idx]["user"]:
+            found = True
+            users_chat_history[idx]["messages"] = user_messages
+            break
+    if not found:
+        users_chat_history.append({"user": user_id, "messages": user_messages})
+
+
+def get_chat_history(user_id: int) -> ChatHistory:
+    """Returns the chat history of the user"""
+    # Initialize message list for chatGPT model
+    if users_chat_history[0]["user"] == UserEnum.UNKNOWN_USER:
+        users_chat_history[0]["user"] = user_id
+        users_chat_history[0]["messages"] = ChatHistory()
+    else:
+        found = False
+        for idx, _ in enumerate(users_chat_history):
+            if user_id == users_chat_history[idx]["user"]:
+                chat_history = users_chat_history[idx]["messages"]
+                found = True
+                break
+        if not found:
+            # found a new user
+            chat_history = ChatHistory()
+            users_chat_history.append({"user": user_id, "messages": chat_history})
+    return chat_history
 
 # start cmd handler
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -56,6 +120,23 @@ async def error(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     print(f"Update {update} causes error {context.error}")
 
 
+# llm response handler
+async def response_handler(user_id: int, prompt: str) -> str:
+    """Response handler
+
+    Args:
+        user_id: user id from Update
+        prompt: user's message
+
+    Returns:
+        response string from llm
+    """
+    chat_history = get_chat_history(user_id)
+    chat_history.append({"role": "user", "content": f"prompt"})
+    response = llm.handle_response(chat_history)
+    update_chat_history(user_id, response)
+    return response[-1]["content"]
+
 # Input message handler
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Message handler
@@ -78,12 +159,12 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     if msg_type == "group":
         if BOT_NAME in msg:
             new_msg = msg.replace(BOT_NAME, "").strip()
-            response = llm.handle_response(user_id, new_msg)
+            response = response_handler(user_id, new_msg)
         else:
             return
     else:
         # private chat
-        response = llm.handle_response(user_id, msg)
+        response = response_handler(user_id, msg)
 
     # print response message for debug
     print(f'Bot: "{response}"')
--- model_openai.py
+++ model_openai.py
@@ -1,15 +1,10 @@
 import openai
 import os
 from dotenv import load_dotenv, find_dotenv
-from enum import Enum
 
 # Get API tokens from environment file
 _ = load_dotenv(find_dotenv())
 openai.api_key = os.getenv("OPENAI_API_TOKEN")
-
-
-class UserEnum(Enum):
-    UNKNOWN_USER = 0
 
 
 class ChatGPT:
@@ -19,14 +14,7 @@ class ChatGPT:
         print("chatGPT initialized.")
         self._name = "chatGPT-3.5"
         self._model = model
-        self._chat_history = [
-            {
-                "user": UserEnum.UNKNOWN_USER,
-                "message": [
-                    {"role": "system", "content": "You are a helpful assistant."}
-                ],
-            }
-        ]
+        self._message = [{"role": "system", "content": "You are a helpful assistant."}]
 
     # a getter function
     @property
@@ -46,34 +34,46 @@ class ChatGPT:
     def name(self, name):
         self._name = name
 
+    @property
+    def message(self):
+        """Return all prompts
+
+        Args:
+            None
+
+        Returns:
+            list of dictionaries
+        """
+        return self._message
+
+    # setter function
+    @message.setter
+    def message(self, msg):
+        self._message = msg
+
     def clear_messages(self, user_id: int) -> None:
         """Clear message list"""
-        conversation = self._get_chat_history(user_id)
-        conversation.clear()
-        conversation.append(
+        self._message.clear()
+        self._message.append(
             {"role": "system", "content": "You are a helpful assistant."}
         )
-        self._update_chat_history(user_id, conversation)
 
-    def handle_response(self, user_id: int, prompt: str) -> str:
+    def handle_response(self, prompt: str) -> list:
         """Retain user messages and append assistant's responses in a message list.
         This is due to the model does not store chat history after each query is sent.
 
         Args:
-            user_id: user id
             prompt: user input message
 
         Returns:
-            response string
+            list of dict of prompts
 
         TODO:
          1) use moderation API to detect hate speech and adhere to safety guidelines
          2) prevent prompt injection attacks
         """
         # retrieve user's message list
-        conversation = self._get_chat_history(user_id)
-        conversation.append({"role": "user", "content": f"{prompt}"})
-
+        conversation = self._message.append({"role": "user", "content": f"{prompt}"})
         response = openai.ChatCompletion.create(
             model=self._model,
             messages=conversation,
@@ -81,42 +81,4 @@ class ChatGPT:
         )
         resp = response.choices[0].message["content"]
         conversation.append({"role": "assistant", "content": f"{resp}"})
-        # update the message list
-        self._update_chat_history(user_id, conversation)
-        return resp
-
-    def _update_chat_history(self, user_id: int, user_messages: list) -> None:
-        """Update chat history of the user"""
-        found = False
-        for idx, _ in enumerate(self._chat_history):
-            if user_id == self._chat_history[idx]["user"]:
-                found = True
-                # shallow copy
-                self._chat_history[idx]["message"] = user_messages
-                break
-        if not found:
-            self._chat_history.append({"user": user_id, "message": user_messages})
-
-    def _get_chat_history(self, user_id: int) -> list:
-        """Returns the chat history of the user"""
-        # Initialize message list for chatGPT model
-        if self._chat_history[0]["user"] == UserEnum.UNKNOWN_USER:
-            self._chat_history[0]["user"] = user_id
-            messages = self._chat_history[0]["message"]
-        else:
-            found = False
-            for idx, _ in enumerate(self._chat_history):
-                if user_id == self._chat_history[idx]["user"]:
-                    messages = self._chat_history[idx]["message"]
-                    found = True
-                    break
-            if not found:
-                # found a new user
-                messages = [
-                    {
-                        "role": "system",
-                        "content": "You are a helpful assistant.",
-                    }
-                ]
-                self._chat_history.append({"user": user_id, "message": messages})
-        return messages
+        return conversation
