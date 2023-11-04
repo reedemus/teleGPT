ab77dd53a3b56df7b271b259c65adb1462bf388b
Terrence Goh <reedemus@yahoo.com.sg>
2023-11-05 00:53:39 +0800

    handle submit button

--- web_app/photo_selection.js
+++ web_app/photo_selection.js
@@ -1,4 +1,9 @@
-function loadImagePreview(inputId) {
+var fileList = {
+    source: "",
+    target: ""
+ };
+
+function loadSourceImagePreview(inputId) {
     var input = document.getElementById(inputId);
     if (input.files && input.files[0]) {
         var reader = new FileReader();
@@ -6,5 +11,24 @@ function loadImagePreview(inputId) {
             document.getElementById('preview' + inputId.charAt(inputId.length - 1)).setAttribute('src', e.target.result);
         }
         reader.readAsDataURL(input.files[0]);
+        fileList["source"] = input.files[0];
+        print(fileList["source"]);
     }
+}
+
+function loadTargetImagePreview(inputId) {
+    var input = document.getElementById(inputId);
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            document.getElementById('preview' + inputId.charAt(inputId.length - 1)).setAttribute('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+        fileList["target"] = input.files[0];
+        print(fileList["target"]);
+    }
+}
+
+function returnFilePath(){
+    window.Telegram.WebApp.sendData(JSON.stringify(fileList));
 }
