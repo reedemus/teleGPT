79926833fe88edf3df878fd9a2266ce4ea0bbdfa
Terrence Goh <reedemus@yahoo.com.sg>
2023-11-05 12:25:36 +0800

    Fix fileList not getting file URL

--- web_app/photo_selection.js
+++ web_app/photo_selection.js
@@ -3,7 +3,7 @@ var fileList = {
     target: ""
  };
 
-function loadSourceImagePreview(inputId) {
+function loadImagePreview(inputId) {
     var input = document.getElementById(inputId);
     if (input.files && input.files[0]) {
         var reader = new FileReader();
@@ -11,21 +11,13 @@ function loadSourceImagePreview(inputId) {
             document.getElementById('preview' + inputId.charAt(inputId.length - 1)).setAttribute('src', e.target.result);
         }
         reader.readAsDataURL(input.files[0]);
-        fileList["source"] = input.files[0];
-        console.log(fileList["source"]);
-    }
-}
-
-function loadTargetImagePreview(inputId) {
-    var input = document.getElementById(inputId);
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-        reader.onload = function(e) {
-            document.getElementById('preview' + inputId.charAt(inputId.length - 1)).setAttribute('src', e.target.result);
+        
+        if (inputId == 'source') {
+            fileList["source"] = URL.createObjectURL(input.files[0]);
         }
-        reader.readAsDataURL(input.files[0]);
-        fileList["target"] = input.files[0];
-        console.log(fileList["target"]);
+        else {
+            fileList["target"] = URL.createObjectURL(input.files[0]);
+        }
     }
 }
 
